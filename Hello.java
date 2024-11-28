f98720fbdcd5363cc99a12d2577c90416545cb15
jayak <kumarijaya127@gmail.com>
2024-11-28 22:02:46 +0530

    hi bye code added

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello{
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
+
+class Bye{
+     public static void main(String[] args){
+        System.out.println("Bye");
+    }
+}
